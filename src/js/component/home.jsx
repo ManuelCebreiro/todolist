e385c51bd9e49c5ad1ca2e813ab56646ac8e0568
ManuelCebreiro <cebreirom@gmail.com>
2022-07-08 19:35:13 +0000

    cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,19 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState} from 'react';
 
 
 export const TodoList = () => {
 	const [list, setlist] = useState([]);
+	const [valor, setvalor] = useState();
 
 	const addList = e => {
 		if (e.key === "Enter" && e.target.value !== "") {
-			setlist([...list, e.target.value]);
+			console.log(e.target.value)
+			for(let i = 0; i < e.target.value.length ; i++){
+				if(e.target.value[i] != " "){
+					setlist([...list, e.target.value]);
+					break
+				}
+			}
 		}
 	};
 	const DeleteItems = (indexItem) => {
@@ -19,10 +26,13 @@ export const TodoList = () => {
 			<h1 className="container d-flex justify-content-center text-center col-8 bi bi-x-lg">TODOS</h1>
 			<div className="container shadow p-1 bg-white rounded col-8 bi bi-x-lg">
 				<input
+					onChange={(e)=>{ setvalor(e.target.value)}}
 					type="text"
 					placeholder="¿Qué tienes que hacer?"
 					className="col-8 border none"
 					onKeyPress={addList}
+					value = {valor}
+					setvalor= {("")}
 				/>
 			</div>
 
